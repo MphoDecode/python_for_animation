df43b1c746d4e08a17d6725f5dab6acb5b1db040
Anele Twala <114112536+Anele0722@users.noreply.github.com>
2024-02-08 11:24:57 +0200

    Update Untitled-1.py

--- .vscode/Untitled-1.py
+++ .vscode/Untitled-1.py
@@ -1,3 +1,17 @@
 name = "Mpho"
 print(name)
-print(name + " is beautiful")
+
+def ask_age():
+    age = input("How old is Mpho? ")
+    return age
+
+def ask_location():
+    location = input("Where does Mpho stay? ")
+    return location
+
+age = ask_age()
+location = ask_location()
+
+print(name + " is " + age + " years old.")
+print(name + " stays in " + location + ".")
+print(name + " is beautiful.")
